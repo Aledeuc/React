8fa9dbfa529649bfdb841827cb93d36f89e85230
aledeuc <aledeuc@sdv.fr>
2021-01-21 17:42:40 +0100

    projet markdown

--- markdown-app/src/App.js
+++ markdown-app/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import marked from 'marked'
+import { sampleText } from './sampleText'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    text: sampleText
+  }
+
+  componentDidMount () {
+    const text = localStorage.getItem('text')
+    if (text) {
+      this.setState({ text })
+    } else {
+      this.setState({ text: sampleText })
+    }
+    
+  }
+
+  componentDidUpdate () {
+    const { text } = this.state
+    localStorage.setItem('text', text )
+  }
+
+  handleChange = event => {
+    const text = event.target.value
+    this.setState({ text })
+  }
+
+  renderText = text =>  {
+    const __html = marked (text, { sanitize: true })
+    return { __html }
+  }
+
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="colonne">
+          <div>
+            <textarea 
+              onChange={this.handleChange}
+              className='textarea' 
+              rows="35"
+              value={ this.state.text } />
+          </div>
+          <div className="result">
+            <div dangerouslySetInnerHTML={ this.renderText(this.state.text) } />
+          </div>
+        </div>
+      </div>
+      
+    );
+  }
 }
 
 export default App;
